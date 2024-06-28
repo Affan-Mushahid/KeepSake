286054599d09612d8da2e71e44a1a6f5123d2ede
Affan Mushahid <affan.mushahid.1@gmail.com>
2024-06-29 01:27:49 +0500

    Created a Login UI

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "KeepSake.h"
+#include <login_menu.h>
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    KeepSake w;
+    //KeepSake w;
+    login_menu w;
     w.show();
     return a.exec();
 
--- user.h
+++ user.h
@@ -35,7 +35,7 @@ public:
 
 	void register_account();
 
-	void sign_in_account();
+	void sign_in();
 };
 
 
