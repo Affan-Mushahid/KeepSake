aef47751b76630402b790b243315c276340b6a9b
Hareem Aqeel <hareemaqeel123@gmail.com>
2024-07-03 21:08:07 +0500

    password generation done

--- pass_generator.cpp
+++ pass_generator.cpp
@@ -1,1 +1,49 @@
 #include <pass_generator.h>
+
+
+Password_Generator::Password_Generator()
+	: lowercase_letters("abcdefghijklmnopqrstuvwxyz")
+	, uppercase_letters("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	, digits("0123456789")
+	, symbols("!@#$%^") {
+
+}
+
+std::string Password_Generator::generate(int size = 8, bool include_symbols = true) {
+	srand(time(NULL));
+
+	std::string key;
+
+	for (int i = 0; i < size; i++) {
+		int choice_category = rand() % 4;
+		int choice_character;
+
+		switch (choice_category) {
+		case 0:
+			choice_character = rand() % (lowercase_letters.size());
+			key += lowercase_letters[choice_character];
+			break;
+
+		case 1:
+			choice_character = rand() % (uppercase_letters.size());
+			key += uppercase_letters[choice_character];
+			break;
+
+		case 2:
+			choice_character = rand() % (digits.size());
+			key += digits[choice_character];
+			break;
+
+		case 3:
+			if(include_symbols == true) {
+				choice_character = rand() % (symbols.size());
+				key += symbols[choice_character];
+			}
+			break;
+		}
+		
+
+	}
+
+	return key;
+}
