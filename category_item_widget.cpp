36a83c7e1442026ca32c7c34e742e0e3d8a203fd
Affan Mushahid <affan.mushahid.1@gmail.com>
2024-07-07 16:31:02 +0500

    commit

---
+++ category_item_widget.cpp
@@ -0,0 +1,14 @@
+#include "category_item_widget.h"
+
+category_item_widget::category_item_widget(std::string category, QWidget *parent)
+	: QWidget(parent)
+	, ui(new Ui::category_item_widgetClass())
+{
+	ui->setupUi(this);
+	ui->label->setText(QString::fromStdString(category));
+}
+
+category_item_widget::~category_item_widget()
+{
+	delete ui;
+}
